10ef0a11eb7cafd44bfa2a366785924519270ef5
Diana B. Gobar <dianabgobar@gmail.com>
2016-11-11 12:54:01 -0500

    model 1 - posts

---
+++ halp/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  resources :posts
+  # get 'posts/' => 'posts#index'
+  # get 'posts/new'
+  # get 'posts/create'
+  # get 'posts/show'
+  #
+  # root 'posts#index'
+  # root 'posts#new'
+  # root 'posts#create'
+  # root 'posts#show'
+end
---
+++ halp/db/seeds.rb
@@ -0,0 +1,4 @@
+Post.destroy_all
+
+post1 = Post.create!(name: "Diana B. Gobar", photo_url: "http://placehold.it/50x50", description: "web dev need halp with resume", resume: "put google docs link here")
+post2 = Post.create!(name: "John Doe", photo_url: "http://placehold.it/50x50", description: "also needs halps..", resume: "put google docs link here")
