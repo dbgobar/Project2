9adc1eadec1516b300b64b6a843ecde508cbc53f
Diana B. Gobar <dianabgobar@gmail.com>
2016-11-16 13:16:48 -0500

    added sessions/users files..halfway through

--- halp/db/seeds.rb
+++ halp/db/seeds.rb
@@ -1,4 +1,5 @@
 Post.destroy_all
+User.destroy_all
 
 post1 = Post.create!(name: "Diana B. Gobar", photo_url: "https://pbs.twimg.com/profile_images/793280191015641088/axS7aP7j.jpg", description: "web dev need halp with resume", resume: "put google docs link here")
 post2 = Post.create!(name: "John Doe", photo_url: "http://www.naturaloil.ph/wp-content/uploads/2015/11/John_Doe.jpg", description: "also needs halps..", resume: "put google docs link here")
