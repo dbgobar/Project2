824739aceabcb68266d806523ca3f5b210459954
Diana B. Gobar <dianabgobar@gmail.com>
2017-01-11 13:04:02 -0500

    added changed files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116141936) do
+ActiveRecord::Schema.define(version: 20161114170539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,13 +31,6 @@ ActiveRecord::Schema.define(version: 20161116141936) do
     t.string   "resume"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "posts"
