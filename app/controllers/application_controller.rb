a54749f477e640ffd06bab360f2670110047722e
Diana B. Gobar <dianabgobar@gmail.com>
2017-01-20 22:52:48 -0500

    added two new migrations, made changes to user input and css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception   # This line should already be in your code.
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :birthday, :email, :password, :remember_me ])
+
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me ])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :birthday, :about, :email, :password, :current_password, :remember_me])
+  end
 end
