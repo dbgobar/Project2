059bbf83e659b8593ea4783d528af80c440d5ba2
Diana B. Gobar <dianabgobar@gmail.com>
2017-01-22 16:13:34 -0500

    changes not made before..added

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
 
   devise_for :users
   resources :posts do
     resources :comments
   end
-
+  resources :users
   root 'posts#index'
- 
+
 
 end
